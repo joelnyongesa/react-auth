18c33292a66a9f41753e6ff79979e80b1f6e5219
joelnyongesa <joelnyongesa148@gmail.com>
2023-12-25 22:12:57 +0300

    Added log in and sign up routes and tokens on the backend

---
+++ back-end/src/routes/index.js
@@ -0,0 +1,9 @@
+import { testRoute } from './testRoute';
+import { signUpRoute } from './signUpRoute';
+import { logInRoute } from './loginRoute';
+
+export const routes = [
+    signUpRoute,
+    logInRoute,
+    testRoute,
+];
